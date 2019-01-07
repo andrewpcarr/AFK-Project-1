2e65ec342a3fc8832496775f82d2777beb37698a
Andrew Carr <andrewpcarr@gmail.com>
2019-01-07 16:45:31 -0600

    Add hardcoded Austin brewery URL

--- app/services/getBreweryData.js
+++ app/services/getBreweryData.js
@@ -2,11 +2,11 @@ import { API_KEYS } from '../../apiKeys';
 
 export function getBreweryData(callback) {
   $.get(getBreweryUrl())
-    .done(callback)
+    .done(response => callback(JSON.parse(response)))
     .fail(error => console.error('AJAX request failed, ', error));
 }
 
 function getBreweryUrl() {
   const city = $('#city-input').val();
-  return `http://api.brewerydb.com/v2/locations/?key=${API_KEYS.BREWERY_DB_KEY}&locality=${city}`;
+  return `https://www.brewerydb.com/browse/map/get-breweries?lat=30.267153&lng=-97.74306079999997&radius=25&search=${city}`;
 }
